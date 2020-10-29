9ec68370949e7df17d33622e8b42c2c617454dd6
sozonome <berserk.nate@gmail.com>
2020-10-29 21:46:25 +0700

    chore: rename project to nextchakra-starter

---
+++ src/components/SomeImage.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/core";
+import Image from "next/image";
+import React from "react";
+
+const SomeImage = () => {
+  return (
+    <Box marginY={8} maxWidth={400}>
+      <Image src="/undraw.png" unsized />
+    </Box>
+  );
+};
+
+export default SomeImage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,25 @@
-import { Flex, Box, Heading } from "@chakra-ui/core";
-import Image from "next/image";
+import { Box, Button, Heading, Link } from "@chakra-ui/core";
+import { AiFillGithub } from "react-icons/ai";
+import SomeImage from "../components/SomeImage";
 
 import SomeText from "../components/SomeText";
 
 const Home = () => {
   return (
-    <Flex mb={8} w="full">
+    <Box mb={8} w="full">
       <Box>
         <Heading as="h2" fontSize="3xl">
           Hello
         </Heading>
         <SomeText />
-        <Box marginY={8} maxWidth={400}>
-          <Image src="/undraw_Status_update_re_dm9y.png" unsized />
-        </Box>
       </Box>
-    </Flex>
+
+      <SomeImage />
+
+      <Link href="https://github.com/sozonome/next-app-chakra-ts" isExternal>
+        <Button leftIcon={AiFillGithub}>Open in Github</Button>
+      </Link>
+    </Box>
   );
 };
 
