deca9b51c5ae63b0643313b353c4467bf43f377a
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-02-14 14:34:21 +0700

    refactor: move meta viewport to document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import Head from "next/head";
 
 import Layout from "../components/layout";
 
@@ -9,12 +8,6 @@ import "../styles/globals.css";
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={customTheme}>
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
-        />
-      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
