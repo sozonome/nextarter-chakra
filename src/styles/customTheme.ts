b2b92bda602cb410a7a67bafc2d306b7443372dd
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-02-20 14:18:56 +0700

    fix(styles): revise customTheme

--- src/styles/customTheme.ts
+++ src/styles/customTheme.ts
@@ -1,7 +1,6 @@
 import { theme, extendTheme } from "@chakra-ui/react";
 
 const customTheme = extendTheme({
-  ...theme,
   fonts: {
     ...theme.fonts,
     /** Example */
@@ -16,6 +15,14 @@ const customTheme = extendTheme({
     //   700: "#005661",
     //   500: "#00838e",
     //   300: "#4fb3be",
+    // },
+  },
+  components: {
+    /** Example */
+    // Button: {
+    //   baseStyle: {
+    //     borderRadius: 24,
+    //   },
     // },
   },
 });
