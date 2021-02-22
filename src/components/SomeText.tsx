3601dadaa852d17c03fab9cd28ca675bdb0f9a61
Vincent Arlou <arlou@protonmail.com>
2021-02-21 21:19:29 -0500

    Fix Div in Text problem:  Warning: Expected server HTML to contain a matching <div> in <p>.

--- src/components/SomeText.tsx
+++ src/components/SomeText.tsx
@@ -16,7 +16,7 @@ const SomeText = () => {
         padding={4}
         borderRadius={4}
       >
-        <Text d="flex" alignItems="center" fontSize="sm">
+        <Box d="flex" alignItems="center" fontSize="sm">
           This is a
           <HelperImage src="/nextjs-black-logo.svg" label="NextJS" />
           app with
@@ -27,7 +27,7 @@ const SomeText = () => {
           and
           <HelperImage src="/ts-logo-512.svg" label="TypeScript" />
           setup.
-        </Text>
+        </Box>
       </Box>
     </>
   );
