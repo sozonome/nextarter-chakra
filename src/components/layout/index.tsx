7473e716eb8c181dad7397aa535b6a66c638eb0d
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-02-28 13:05:50 +0700

    fix: revise typing

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,15 @@
 import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 import Header from "./Header";
 import Footer from "./Footer";
 import Meta from "./Meta";
 
-const Layout = ({ children }) => {
+type LayoutProps = {
+  children: ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
   return (
     <Box margin="0 auto" maxWidth={800} transition="0.5s ease-out">
       <Meta />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
 import Head from "next/head";
 
 import Layout from "../components/layout";
@@ -6,7 +7,7 @@ import Layout from "../components/layout";
 import customTheme from "../styles/customTheme";
 import "../styles/globals.css";
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={customTheme}>
       <Head>
