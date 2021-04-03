2cbdc8854f9524e25fd1bdd076ff766b164fac5d
imad <70093484+imadatyatalah@users.noreply.github.com>
2021-04-03 15:49:13 +0100

    Fix some Accessibility issues (#14)
    * Added alt attribute and discernible name to links
    
    * Added alt attribute and Remove unnecessary Box component
    
    * Added lang attribute
    
    * Added alt attribute

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -16,7 +16,10 @@ const Page404 = () => {
         width={["100%", "70%", "60%", "60%"]}
         margin="0 auto"
       >
-        <Image src="/404 Error-pana.svg" />
+        <Image
+          src="/404 Error-pana.svg"
+          alt="Error 404 not found Illustration"
+        />
       </MotionBox>
       <Text textAlign="center" fontSize="xs">
         <ChakraLink href="https://stories.freepik.com/web" isExternal>
