9f2d74cb57235c6e66967d1994b6307b19b44b99
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-08-17 00:13:38 +0700

    chore: adjust commitlint config to use typescript

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
-module.exports = {
+/** @type {import('@commitlint/types').UserConfig} */
+
+const CommitLintConfiguration = {
   extends: ["@commitlint/config-conventional"],
 };
+
+module.exports = CommitLintConfiguration;
