923d0f9a538fcc0c918456d17764591bec172732
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-09-02 00:09:29 +0700

    chore: add commitlint notes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@commitlint/types').UserConfig} */
 const CommitLintConfiguration = {
   extends: ["@commitlint/config-conventional"],
+  // add your own scope here if needed
+  // "scope-enum": [2, "always", ["components", "pages", "utils"]],
+  "scope-case": [2, "always", "kebab-case"],
 };
 
 module.exports = CommitLintConfiguration;
