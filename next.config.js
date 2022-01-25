bf485a82654c1c21db29c895ec9f13048dd722f9
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-01-25 19:15:01 +0700

    chore: set custom dir for next eslint
    References:
    https://nextjs.org/docs/basic-features/eslint#linting-custom-directories-and-files

--- next.config.js
+++ next.config.js
@@ -8,11 +8,14 @@ module.exports = withPWA({
       process.env.NODE_ENV === "development" ||
       process.env.NODE_ENV === "preview" ||
       process.env.NODE_ENV === "production",
-      // delete two lines above to enable PWA in production deployment
-      // add your own icons to public/manifest.json 
-      // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
+    // delete two lines above to enable PWA in production deployment
+    // add your own icons to public/manifest.json
+    // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
     dest: "public",
     register: true,
   },
   reactStrictMode: true,
+  eslint: {
+    dirs: ["src"],
+  },
 });
