a026efe970d8c81d5e70d5fe0246213fe2594946
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-04-22 23:59:27 +0700

    chore: setup commitizen

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,15 @@
 /** @type {import('@commitlint/types').UserConfig} */
 const CommitLintConfiguration = {
   extends: ["@commitlint/config-conventional"],
-  // add your own scope here if needed
-  // "scope-enum": [2, "always", ["components", "pages", "utils"]],
-  "scope-case": [2, "always", "kebab-case"],
+  rules: {
+    // add your own scope here if needed
+    "scope-enum": [
+      2,
+      "always",
+      ["components", "layout", "pages", "styles", "utils", "types"],
+    ],
+    "scope-case": [2, "always", "kebab-case"],
+  },
 };
 
 module.exports = CommitLintConfiguration;
