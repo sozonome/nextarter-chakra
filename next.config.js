78380355f989d536ae279bf67053f80fec176c56
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-08-02 21:36:31 +0700

    chore: remove next-pwa config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  pwa: {
-    disable:
-      process.env.NODE_ENV === "development" ||
-      process.env.NODE_ENV === "preview" ||
-      process.env.NODE_ENV === "production",
-    // delete two lines above to enable PWA in production deployment
-    // add your own icons to public/manifest.json
-    // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
-    dest: "public",
-    register: true,
-  },
   swcMinify: true,
   reactStrictMode: true,
   eslint: {
