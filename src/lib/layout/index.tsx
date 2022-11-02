6e7fcf9b1fb3eb041f719db4b27890ecca862816
norris <33255034+Gsirron@users.noreply.github.com>
2022-11-02 15:32:26 +1100

    chore: added some bg items

--- src/lib/layout/index.tsx
+++ src/lib/layout/index.tsx
@@ -1,6 +1,7 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import type { ReactNode } from "react";
 
+import ConfettiBg from "./backgrounds/ConfettiBg";
 import Footer from "./Footer";
 import Header from "./Header";
 
@@ -9,8 +10,15 @@ type LayoutProps = {
 };
 
 const Layout = ({ children }: LayoutProps) => {
+  const { CONFETTI_LIGHT, CONFETTI_DARK } = ConfettiBg;
+
   return (
-    <Box margin="0 auto" maxWidth={800} transition="0.5s ease-out">
+    <Box
+      bg={useColorModeValue(CONFETTI_LIGHT, CONFETTI_DARK)}
+      margin="0 auto"
+      maxWidth={800}
+      transition="0.5s ease-out"
+    >
       <Box margin="8">
         <Header />
         <Box as="main" marginY={22}>
