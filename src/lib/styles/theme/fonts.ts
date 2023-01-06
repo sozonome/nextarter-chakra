195bbc6c37dd1904673ecda15b49fe92018ff7e5
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-01-06 20:40:16 +0700

    refactor: update default font to figtree

--- src/lib/styles/theme/fonts.ts
+++ src/lib/styles/theme/fonts.ts
@@ -1,6 +1,6 @@
 import type { DeepPartial, Theme } from "@chakra-ui/react";
 
 export const fonts: DeepPartial<Theme["fonts"]> = {
-  body: "Lexend, sans-serif",
-  heading: "Lexend, serif",
+  body: `"Figtree", sans-serif`,
+  heading: `"Figtree", serif`,
 };
