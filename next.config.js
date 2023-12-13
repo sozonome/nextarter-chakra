904c01d64b2bd080bd2e1b27f0ce4466667b39fc
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-12-13 19:43:33 +0700

    chore: enable PWA by default

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable:
-    process.env.NODE_ENV === "development" ||
-    process.env.NODE_ENV === "preview" ||
-    process.env.NODE_ENV === "production",
-  // delete two lines above to enable PWA in production deployment
-  // add your own icons to public/manifest.json
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  // add your own icons to src/app/manifest.ts
   // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
 });
 
@@ -14,6 +10,6 @@ module.exports = withPWA({
   swcMinify: true,
   reactStrictMode: true,
   eslint: {
-    dirs: ["src"],
+    dirs: ['src'],
   },
 });
