79720053871b55bc939961c63c7826486693adf9
Agustinus Nathaniel <berserk.nate@gmail.com>
2024-10-28 19:10:20 +0700

    refactor: remove api route

--- src/app/api/hello/route.ts
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { NextResponse } from 'next/server';
-
-export const GET = () => {
-  return NextResponse.json({ name: 'John Doe' }, { status: 200 });
-};
